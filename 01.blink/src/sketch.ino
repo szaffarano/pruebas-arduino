ae800dd48d49a77175cfead8ff6cef921943fbeb
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2012-11-03 11:07:18 -0300

    agrego .gitignore y estilo del código

--- 01.blink/src/sketch.ino
+++ 01.blink/src/sketch.ino
@@ -1,13 +1,10 @@
-
 #define LED_PIN 13
 
-void setup()
-{
+void setup() {
     pinMode(LED_PIN, OUTPUT);
 }
 
-void loop()
-{
+void loop() {
     digitalWrite(LED_PIN, HIGH);
     delay(100);
     digitalWrite(LED_PIN, LOW);
